7f3d38c83129eca655b21decfe25389f47cfc56d
Ronil Bhatia <ronilbhatia@gmail.com>
2018-12-23 15:27:05 -0800

    Finish node web server project

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -19,9 +19,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -49,6 +49,12 @@ app.get('/', (req, res) => {
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle this request'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects',
   });
 });
 
